59314e72030366ee1284b0a98a3d0b95ea93835f
Allori <wlad70657@gmail.com>
2023-10-11 10:09:13 +0300

    Updates

--- assets/FCloseIcon.stories-cffef690.js
+++
@@ -1,4 +0,0 @@
-import{a as c}from"./jsx-runtime-03b4ddbf.js";/* empty css                  */import{F as a}from"./FCloseIcon-149fc16a.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";/* empty css               */const g={title:"Icons/FCloseIcon",tags:["autodocs"],component:a},n=t=>c(a,{...t}),o=n.bind({});o.args={id:"close-icon-id",className:"close-icon-className",st:{padding:"20px"},handleClose:()=>{alert("Клик")}};var s,e,r;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`args => {
-  return <FCloseIcon {...args} />;
-}`,...(r=(e=o.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const f=["Default"];export{o as Default,f as __namedExportsOrder,g as default};
-//# sourceMappingURL=FCloseIcon.stories-cffef690.js.map
