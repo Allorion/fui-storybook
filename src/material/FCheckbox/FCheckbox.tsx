18b919a4cd152081603ba788d233096421e86e64
RWEC\svetashev_va <allori@allorion.ru>
2024-08-12 17:21:15 +0300

    Добавил базовые методы в чекбокс, исправил анимации в выпадающем списке кнопки Dropdown

--- src/material/FCheckbox/FCheckbox.tsx
+++ src/material/FCheckbox/FCheckbox.tsx
@@ -1,44 +1,35 @@
-import React, {FC} from "react";
+import React, { forwardRef } from "react";
 import "./Checkbox.css"
 
 export interface IFCheckbox {
     label?: React.ReactChild | React.ReactNode,
-    onChange: React.ChangeEventHandler<HTMLInputElement> | undefined
-    defaultChecked?: boolean | undefined
     className?: string
     id?: string
     st?: React.CSSProperties | undefined,
-    checked?: boolean | undefined,
-    disabled?: boolean
 }
 
-const FCheckbox: FC<IFCheckbox> = ({
-                                       label,
-                                       onChange,
-                                       defaultChecked,
-                                       className,
-                                       id,
-                                       st,
-                                       checked,
-                                       disabled
-                                   }) => {
+const FCheckbox = forwardRef<HTMLInputElement, IFCheckbox>(({
+    label,
+    className,
+    id,
+    st,
+    ...props
+}, ref) => {
 
     return (
         <React.Fragment>
             <div className={`f-checkbox ${className !== undefined ? className : ''}`} id={id} style={st}>
                 <label>
                     <input
-                        defaultChecked={defaultChecked}
+                        ref={ref}
                         type="checkbox"
-                        onChange={onChange}
-                        checked={checked}
-                        disabled={disabled}
+                        {...props}
                     />
                     {label}
                 </label>
             </div>
         </React.Fragment>
     )
-}
+});
 
 export default FCheckbox;
