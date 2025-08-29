0043c9cd16816ba81ac86879ba161e98fa07c385
Allori <allori@allorion.ru>
2025-08-29 14:05:52 +0300

    Updates

---
+++ assets/index-B_tDNPrG.js
@@ -0,0 +1,1 @@
+import"./preview-DSSbnTGf.js";import"./DocsRenderer-CFRXHY34-C9Ugl-C7.js";import"./index-DZcZBAEO.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
--- assets/index-BmJ0ifIO.js
+++
@@ -1,1 +0,0 @@
-import"./preview-AwcgmnsL.js";import"./DocsRenderer-CFRXHY34-CwDi_R9h.js";import"./index-W11HQexB.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
