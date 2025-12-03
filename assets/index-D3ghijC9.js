a8de0c324be964662fb5904b4ef3f4d9b46abb3d
Allori <allori@allorion.ru>
2025-12-03 11:59:08 +0500

    Updates

--- assets/index-D3ghijC9.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Dae6In-b.js";import"./DocsRenderer-CFRXHY34-BR7xeYUn.js";import"./index-D4zPF2uo.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
---
+++ assets/index-DwoY1sTV.js
@@ -0,0 +1,1 @@
+import"./preview-hnw76WLk.js";import"./DocsRenderer-CFRXHY34-CFVtM4HX.js";import"./index-DEYUkSjZ.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
