4c5679f295592e6ab0fddd0a02708dddd370017e
DESKTOP-HO5JLJE\Allori <allori@allorion.ru>
2026-02-16 15:07:27 +0300

    Updates

---
+++ assets/index-BD4DI06v.js
@@ -0,0 +1,1 @@
+import"./preview-hIjkJ1PH.js";import"./DocsRenderer-CFRXHY34-DyOHw2nH.js";import"./index-DDGA6GhJ.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
--- assets/index-CuTq1PjY.js
+++
@@ -1,1 +0,0 @@
-import"./preview-BJjcpRzj.js";import"./DocsRenderer-CFRXHY34-Ci879hBD.js";import"./index-C8JlcfbT.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
