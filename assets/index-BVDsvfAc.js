c1df106941771c454b2a92224a1c351fc834db1c
DESKTOP-HO5JLJE\Allori <allori@allorion.ru>
2026-02-24 09:04:38 +0300

    Updates

--- assets/index-BVDsvfAc.js
+++
@@ -1,1 +0,0 @@
-import"./preview-Dg72giOB.js";import"./DocsRenderer-CFRXHY34-DNNrzaoh.js";import"./index-C3PkEww2.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
---
+++ assets/index-DALg5sRL.js
@@ -0,0 +1,1 @@
+import"./preview-DwCCFJlV.js";import"./DocsRenderer-CFRXHY34-DThKL4LD.js";import"./index-CavgetNk.js";const{definePreview:o}=__STORYBOOK_MODULE_PREVIEW_API__;
